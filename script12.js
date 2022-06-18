1237f2b8666607d3fdcc4bc8a13533ee8c1640a2
Chedlyrdissi <chedlyrd2000@gmail.com>
2022-06-17 22:31:14 -0400

    clean ups and minor suggestions

--- script12.js
+++ script12.js
@@ -1,24 +1,11 @@
  // api url
-const api_url =
-	"https://d1s141fbt1.execute-api.us-east-1.amazonaws.com/dev/vehicles";
+const api_url = "https://d1s141fbt1.execute-api.us-east-1.amazonaws.com/dev/vehicles";
 
 async function fetchData(url) {
 	return new Promise((resolve, reject) => {
-		const headers = new Headers();
-		headers.append("Access-Control-Allow-Origin", "*");
-		headers.append("Access-Control-Allow-Credentials", true);
-		headers.append("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
-		headers.append("Access-Control-Allow-Headers", "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
-
-// header("Access-Control-Allow-Origin: *");
-// header("Access-Control-Allow-Credentials: true ");
-// header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
-// header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
-
 		const requestOptions = {
 			method: 'GET',
 			redirect: 'follow',
-			headers,
 		};
 
 		fetch(url, requestOptions)
@@ -30,15 +17,14 @@ async function fetchData(url) {
 
 // Defining async function
 async function getapi(url) {
-	
-	// Storing response
-	// const response = await fetch (url,{mode: 'no-cors'});
 	try {
 		const data = await fetchData(url);
 		show(data);
 	} catch (error) {
 		console.log('Failed to fetch data', error);
 	}
+	// Storing response
+	// const response = await fetch (url,{mode: 'no-cors'});
 	// // Storing data in form of JSON
 	// var data = await response.json();
 	// console.log(data);	
